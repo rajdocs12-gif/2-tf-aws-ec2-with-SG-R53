807ccb2e6014db9648cd2950fddcb3d39a2e7e2b
raj-devops <rajdocs12@gmail.com>
2026-03-26 08:26:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "ami_id" {
+  type = string #optional
+  default = ami-09c813fb71547fc4f
+  swscription = " this is the 
+}
+
+variable "instance_type" {
+type = string
+default = "t3.micro"
+}
+
+variable "sg_name" {
+  default = "allow_ssh"
+}
+
+variable "environment" {
+    default = "prod"
+}
+
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "devops"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+variable "zone_id" {
+    default = "Z013336423EMEFXADOVCI"
+}
+
+variable "domain_name" {
+    default = "rajaws.online"
+}
